c8989838289d9fd24969b0a54c8e4ff677e25581
qrattendanceproject <qrattendanceproject@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-09 00:56:42 +0000

    Create most of JSP/java for handling creating QR codes for conferences an individual will attend.

---
+++ trunk/QRAProject/src/com/qra/project/QRData.java
@@ -0,0 +1,91 @@
+package com.qra.project;
+
+public class QRData {
+
+	private String confName;
+	private String attName;
+	private String userID;
+	private String confID;
+	private String dates;
+	
+	/***
+	 * Initialize an object that has all data needed for a QR ID to be printed.
+	 * @param cName
+	 * @param aName
+	 * @param uID
+	 * @param cID
+	 * @param d
+	 */
+	public QRData(String cName, String aName, String uID, String cID, String d){
+		confName=cName;
+		attName=aName;
+		userID=uID;
+		confID=cID;
+		dates=d;
+	}
+	
+	/**
+	 * @return the confName
+	 */
+	public String getConfName() {
+		return confName;
+	}
+	/**
+	 * @param confName the confName to set
+	 */
+	public void setConfName(String confName) {
+		this.confName = confName;
+	}
+	/**
+	 * @return the attName
+	 */
+	public String getAttName() {
+		return attName;
+	}
+	/**
+	 * @param attName the attName to set
+	 */
+	public void setAttName(String attName) {
+		this.attName = attName;
+	}
+	/**
+	 * @return the userID
+	 */
+	public String getUserID() {
+		return userID;
+	}
+	/**
+	 * @param userID the userID to set
+	 */
+	public void setUserID(String userID) {
+		this.userID = userID;
+	}
+	/**
+	 * @return the confID
+	 */
+	public String getConfID() {
+		return confID;
+	}
+	/**
+	 * @param confID the confID to set
+	 */
+	public void setConfID(String confID) {
+		this.confID = confID;
+	}
+	/**
+	 * @return the dates
+	 */
+	public String getDates() {
+		return dates;
+	}
+	/**
+	 * @param dates the dates to set
+	 */
+	public void setDates(String dates) {
+		this.dates = dates;
+	}
+	
+	
+	
+	
+}
