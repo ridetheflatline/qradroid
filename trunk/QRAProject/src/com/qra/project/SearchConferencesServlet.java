758db6531f0c9007a6b697cb12232bdd0846fd43
jamesffielder@gmail.com <jamesffielder@gmail.com@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-15 22:55:20 +0000

    stuff

--- trunk/QRAProject/src/com/qra/project/SearchConferencesServlet.java
+++ trunk/QRAProject/src/com/qra/project/SearchConferencesServlet.java
@@ -2,6 +2,8 @@ package com.qra.project;
 
 import java.io.IOException;
 
+import javax.jdo.PersistenceManager;
+import javax.jdo.Query;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -17,11 +19,20 @@ public class SearchConferencesServlet extends HttpServlet {
 		String cityOrZip = req.getParameter("cityOrZip");
 		String state = req.getParameter("state");
 		
+		//Currently just search for the conf_name and conf_description
+		//TODO: add geographical searching
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query q = pm.newQuery(Conference.class);
 		
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException 
 	{
+		
+	}
+	
+	//Temporary placeholder to return some conferences
+	public static void testReturnConferences(){
 		
 	}
 
