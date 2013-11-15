1325e2bdf5ad99e4800ea02ff1bab7dfcbe5a8cf
jamesffielder@gmail.com <jamesffielder@gmail.com@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-15 23:38:10 +0000

    find conferences servlet

--- trunk/QRAProject/src/com/qra/project/SearchConferencesServlet.java
+++ trunk/QRAProject/src/com/qra/project/SearchConferencesServlet.java
@@ -1,6 +1,9 @@
 package com.qra.project;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
 
 import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
@@ -9,20 +12,24 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.mortbay.log.Log;
+
 public class SearchConferencesServlet extends HttpServlet {
+	
+    private static final Logger log = Logger.getLogger(CheckinAttendentServlet.class.getName());
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res)
 			throws IOException 
 	{
 		String searchString = req.getParameter("search");
-		String radius = req.getParameter("radius");
-		String cityOrZip = req.getParameter("cityOrZip");
+		String city = req.getParameter("city");
 		String state = req.getParameter("state");
 		
 		//Currently just search for the conf_name and conf_description
 		//TODO: add geographical searching
-		PersistenceManager pm = PMF.get().getPersistenceManager();
-		Query q = pm.newQuery(Conference.class);
+		
+		//generatE pages for joining conferences
+		List<Conference> conferences = testReturnConferences();
 		
 	}
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
@@ -32,8 +39,15 @@ public class SearchConferencesServlet extends HttpServlet {
 	}
 	
 	//Temporary placeholder to return some conferences
-	public static void testReturnConferences(){
+	public static List<Conference> testReturnConferences(){
+		PersistenceManager pm = PMF.get().getPersistenceManager();
+		Query q = pm.newQuery(Conference.class);
 		
+		List<Conference> conferences = (List <Conference>)q.execute();
+		log.info("number of conferences: " +conferences.size());
+		
+		q.closeAll();
+		return conferences;
 	}
 
 }
