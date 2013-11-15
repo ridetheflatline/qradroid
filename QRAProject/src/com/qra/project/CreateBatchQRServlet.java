99cbe098dccf03651b3bfb55ee9d70437ae0c816
qrattendanceproject <qrattendanceproject@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-15 23:52:33 +0000

    Complete QR creation for testing

--- QRAProject/src/com/qra/project/CreateBatchQRServlet.java
+++ QRAProject/src/com/qra/project/CreateBatchQRServlet.java
@@ -18,51 +18,33 @@ public class CreateBatchQRServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-		String userName = req.getParameter("username");
+		String confID = req.getParameter("conf_id");
 		String fullName = null;
 		String userID = null;
-		String confID=null;
+//		String confID=null;
 		String confName=null;
-
-		try {
-
-			Query q = pm.newQuery(Conference.class, "host_ID == '" + userName+ "'");
-
-			List<Conference> confResults = (List<Conference>) q.execute();
-
-//			resp.getWriter().print(""+results.get(0).getID());
-			ArrayList<QRData> qrData = new ArrayList<QRData>();
-			
-			for(int i=0; i<confResults.size();i++){
-				confID=confResults.get(i).getConf_code();
-				confName=confResults.get(i).getConf_name();
-				Query q2 = pm.newQuery(ConferenceAttendee.class, "conf_code == '" +confID+ "'");
-				List<ConferenceAttendee> attResults = (List<ConferenceAttendee>) q2.execute();
-				
-				User tempUser=null;
-				for(int j=0;j<attResults.size();j++){
-					userID=attResults.get(j).getUser_id();
-					tempUser=pm.getObjectById(User.class, userID);
-					fullName=tempUser.getFirst_name()+" "+tempUser.getLast_name();
-					qrData.add(new QRData(confName,fullName,userID,confID,"test"));
-				}
-				
-			}
-			
-			
-
-			
-			req.setAttribute("qrarray", qrData);
-			String url = "/createmyqr.jsp";
-			RequestDispatcher dispatcher=getServletContext().getRequestDispatcher(url);
-			dispatcher.forward(req, resp);
-			
-		} catch (JDOObjectNotFoundException e) {
-			resp.getWriter()
-					.print("We are unable to find the user with the given username and password");
-			resp.getWriter().print(
-					"<br> <a href=\"login.jsp\">Return to Log In</a>");
+		Conference tempConf=null;
+		User tempUser=null;
+		ArrayList<QRData> qrData = new ArrayList<QRData>();
+		
+		
+		tempConf=pm.getObjectById(Conference.class, confID);
+		
+		Query q = pm.newQuery(ConferenceAttendee.class, "conf_code == '" +confID+ "'");
+		List<ConferenceAttendee> attResults = (List<ConferenceAttendee>) q.execute();
+		
+		
+		for(int j=0;j<attResults.size();j++){
+			userID=attResults.get(j).getUser_id();
+			tempUser=pm.getObjectById(User.class, userID);
+			fullName=tempUser.getFirst_name()+" "+tempUser.getLast_name();
+			qrData.add(new QRData(tempConf.getConf_name(),fullName,userID,confID,"test"));
 		}
+		req.setAttribute("qrarray", qrData);
+		String url = "/createmyqr.jsp";
+		RequestDispatcher dispatcher=getServletContext().getRequestDispatcher(url);
+		dispatcher.forward(req, resp);
+		
 	}
 	
 	
--- QRAProject/src/com/qra/project/CreateMyQRServlet.java
+++ QRAProject/src/com/qra/project/CreateMyQRServlet.java
@@ -12,53 +12,36 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class CreateMyQRServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException, ServletException {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
-		String userName = req.getParameter("username");
+		String confID = req.getParameter("conf_id");
 		String fullName = null;
 		String userID = null;
+		HttpSession session = req.getSession();
+		String userName=(String)session.getAttribute("userSess");
+		Conference tempConf=null;
+		ArrayList<QRData> qrData = new ArrayList<QRData>();
+		
+		Query q = pm.newQuery(User.class, "username == '" + userName+ "'");
 
-		try {
-
-			Query q = pm.newQuery(User.class, "username == '" + userName+ "'");
-
-			List<User> results = (List<User>) q.execute();
-
-			resp.getWriter().print(
-					""+results.get(0).getID());
-			
-			fullName=results.get(0).getFirst_name()+" "+results.get(0).getLast_name();
-			userID=results.get(0).getID();
-			
-			
-			Query q2 = pm.newQuery(ConferenceAttendee.class, "user_id == '" +userID+ "'");
-			
-			List<ConferenceAttendee> attResults = (List<ConferenceAttendee>) q2.execute();
-			String confID=null;
-			Conference tempConf=null;
-			ArrayList<QRData> qrData = new ArrayList<QRData>();
-			
-			for (int i=0;i<attResults.size();i++){
-				confID=attResults.get(i).getConf_code();
-				tempConf = pm.getObjectById(Conference.class, confID);
-				qrData.add(new QRData(tempConf.getConf_name(),fullName,userID,confID,"test"));
-				
-			}
-			
-			req.setAttribute("qrarray", qrData);
-			String url = "/createmyqr.jsp";
-			RequestDispatcher dispatcher=getServletContext().getRequestDispatcher(url);
-			dispatcher.forward(req, resp);
-			
-		} catch (JDOObjectNotFoundException e) {
-			resp.getWriter()
-					.print("We are unable to find the user with the given username and password");
-			resp.getWriter().print(
-					"<br> <a href=\"login.jsp\">Return to Log In</a>");
-		}
+		List<User> results = (List<User>) q.execute();
+		
+		fullName=results.get(0).getFirst_name()+" "+results.get(0).getLast_name();
+		userID=results.get(0).getID();
+		tempConf=pm.getObjectById(Conference.class, confID);
+		qrData.add(new QRData(tempConf.getConf_name(),fullName,userID,confID,"test"));
+		
+		
+		req.setAttribute("qrarray", qrData);
+		String url = "/createmyqr.jsp";
+		RequestDispatcher dispatcher=getServletContext().getRequestDispatcher(url);
+		dispatcher.forward(req, resp);
+		
+		
 	}
 }
