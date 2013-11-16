8967b4dd904c75782dc13934cfb21d57423acf2c
qrattendanceproject <qrattendanceproject@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-16 00:54:54 +0000

    Add checks to CreateQRServlet

--- QRAProject/src/com/qra/project/CreateQRServlet.java
+++ QRAProject/src/com/qra/project/CreateQRServlet.java
@@ -24,38 +24,39 @@ public class CreateQRServlet extends HttpServlet {
 		Query q = pm.newQuery(User.class, "username == '" + userName+ "'");
 
 		List<User> results = (List<User>) q.execute();
-
-		resp.getWriter().print(""+results.get(0).getID());
-		
-//		fullName=results.get(0).getFirst_name()+" "+results.get(0).getLast_name();
 		userID=results.get(0).getID();
 		
 		//Get Conferences user is signed up to attend
 		Query q2 = pm.newQuery(ConferenceAttendee.class, "user_id == '" +userID+ "'");
 		
 		List<ConferenceAttendee> attResults = (List<ConferenceAttendee>) q2.execute();
-		String confID=null;
-		Conference tempConf=null;
-		ArrayList<Conference> attQRData = new ArrayList<Conference>();
-		
-		for (int i=0;i<attResults.size();i++){
-			confID=attResults.get(i).getConf_code();
-			tempConf = pm.getObjectById(Conference.class, confID);
-			attQRData.add(tempConf);
+		if (attResults!=null){
+			String confID=null;
+			Conference tempConf=null;
+			ArrayList<Conference> attQRData = new ArrayList<Conference>();
+			
+			for (int i=0;i<attResults.size();i++){
+				confID=attResults.get(i).getConf_code();
+				tempConf = pm.getObjectById(Conference.class, confID);
+				attQRData.add(tempConf);
+			}
+			req.setAttribute("attqrarray", attQRData);
 		}
-		req.setAttribute("attqrarray", attQRData);
 		
 		
 		//Get Conferences user is hosting
 		Query q3 = pm.newQuery(Conference.class, "host_ID == '" + userName+ "'");
 
 		List<Conference> confResults = (List<Conference>) q3.execute();
-		ArrayList<Conference> hostQRData = new ArrayList<Conference>();
 		
-		for(int j=0;j<confResults.size();j++){
-			hostQRData.add(confResults.get(j));
+		if(confResults!=null){
+			ArrayList<Conference> hostQRData = new ArrayList<Conference>();
+			
+			for(int j=0;j<confResults.size();j++){
+				hostQRData.add(confResults.get(j));
+			}
+			req.setAttribute("hostqrarray", hostQRData);
 		}
-		req.setAttribute("hostqrarray", hostQRData);
 	
 		
 		String url = "/createqr.jsp";
