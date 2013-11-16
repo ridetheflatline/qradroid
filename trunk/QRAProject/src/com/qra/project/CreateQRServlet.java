e968106d40a3f13e3ec24608edc7460887ac6d07
qrattendanceproject@gmail.com <qrattendanceproject@gmail.com@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-16 21:30:59 +0000

    CreateQRServlet edit

--- trunk/QRAProject/src/com/qra/project/CreateQRServlet.java
+++ trunk/QRAProject/src/com/qra/project/CreateQRServlet.java
@@ -8,6 +8,7 @@ import javax.jdo.PersistenceManager;
 import javax.jdo.Query;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +18,40 @@ public class CreateQRServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		PersistenceManager pm = PMF.get().getPersistenceManager();
+		/*
 		HttpSession session = req.getSession();
 		String userName=(String)session.getAttribute("userSess");
+		*/
+		String userName;
 		String userID=null;
+		
+		Cookie[] cookies = req.getCookies();
+		String cookieName = "userIDCookie";
+		String cookieValue = "";
+		if (cookies != null)//if there are cookies
+		{
+			for (int i = 0; i < cookies.length; i++) //search through the cookies
+			{
+				Cookie cookie = cookies[i]; //take one cookie
+
+				if (cookieName.equals(cookie.getName()))// is it the cookie we are looking for?
+				{
+					cookieValue = cookie.getValue(); //cookieValue is username 
+				}
+				else
+				{
+					if(i == cookies.length-1 && cookieValue != null) //If it's the last cookie and the userIdCookie is still null
+					{
+						resp.getWriter().print("You're not logged in.<br>");
+					}
+				}
+			}
+		}
+		else {
+			resp.getWriter().print("<br>You're not logged in.<br>");
+		}
+		
+		userName = cookieValue;
 
 		Query q = pm.newQuery(User.class, "username == '" + userName+ "'");
 
@@ -31,6 +63,7 @@ public class CreateQRServlet extends HttpServlet {
 		
 		List<ConferenceAttendee> attResults = (List<ConferenceAttendee>) q2.execute();
 		if (attResults.size()>0){
+			
 			String confID=null;
 			Conference tempConf=null;
 			ArrayList<Conference> attQRData = new ArrayList<Conference>();
@@ -41,7 +74,11 @@ public class CreateQRServlet extends HttpServlet {
 				attQRData.add(tempConf);
 			}
 			req.setAttribute("attqrarray", attQRData);
+			
+			resp.getWriter().print("attResults.size() is greater than 0 <br>");
 		}
+		else
+			resp.getWriter().print("No results for attResults.size()");
 		
 		
 		//Get Conferences user is hosting
@@ -50,13 +87,18 @@ public class CreateQRServlet extends HttpServlet {
 		List<Conference> confResults = (List<Conference>) q3.execute();
 		
 		if(confResults.size()>0){
+			
 			ArrayList<Conference> hostQRData = new ArrayList<Conference>();
 			
 			for(int j=0;j<confResults.size();j++){
 				hostQRData.add(confResults.get(j));
 			}
 			req.setAttribute("hostqrarray", hostQRData);
+			
+			resp.getWriter().print("confResults.size()>0\"<br>");
 		}
+		else
+			resp.getWriter().print("No results for confResults.size()>0");
 	
 		
 		String url = "/createqr.jsp";
