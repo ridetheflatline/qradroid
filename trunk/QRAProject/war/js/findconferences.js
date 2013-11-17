c7b713158c5a06d4949b7c4d6b79a6b8d86e8380
jamesffielder@gmail.com <jamesffielder@gmail.com@46f55a48-31d8-6c2b-38d9-c1b5235c93de>
2013-11-17 00:33:35 +0000

    search and join conferences and stuff

--- trunk/QRAProject/war/js/findconferences.js
+++ trunk/QRAProject/war/js/findconferences.js
@@ -2,6 +2,31 @@ var search = $("#search");
 var state = $("#state");
 var city = $("#city");
 
+function getCookie(c_name)
+{
+	var c_value = document.cookie;
+	var c_start = c_value.indexOf(" " + c_name + "=");
+	if (c_start == -1)
+	{
+		c_start = c_value.indexOf(c_name + "=");
+	}
+	if (c_start == -1)
+	{
+		c_value = null;
+	}
+	else
+	{
+		c_start = c_value.indexOf("=", c_start) + 1;
+		var c_end = c_value.indexOf(";", c_start);
+		if (c_end == -1)
+		{
+			c_end = c_value.length;
+		}
+		c_value = unescape(c_value.substring(c_start,c_end));
+	 }
+	return c_value;
+}
+
 $("#findBtn").button().click(function(){
 	console.log("search: " + search.val());
 	console.log("state: " + state.val());
@@ -25,7 +50,15 @@ function generateHTMLResults(data){
 		var appendValues = $("<tr>").append("<td>" + dataArray[i]["conf_name"] + "</td>");
 		appendValues.append("<td>"+dataArray[i]["conf_descrip"] + "</td>");
 		appendValues.append("<td>"+dataArray[i]["numSessions"]+"</td>");
-		var joinHref = "google.com";
+		//conf_code is conf_id
+		var joinHref = "/joinconference?conf_code=" + dataArray[i]["conf_id"] + "&page_output=true";
+		
+		console.log(getCookie("userKeyId"));
+		
+		if(getCookie("userKeyId") != null){
+			joinHref += "&user_id=" + getCookie("userKeyId");
+		}
+		
 		appendValues.append($("<td>").append($("<a>").text("Join").attr("href",joinHref)));
 		var moreInfoHref = "/moreconfinfo?conf_id=" + dataArray[i]["conf_id"] + "&page_output=true";
 		appendValues.append($("<td>").append($("<a>").text("More Information").attr("href",moreInfoHref)));
